c155ccafa1e553a5cc7c3d6fda140b45e1cc38b6
Matt <mmccollow@gmail.com>
2012-11-26 13:12:39 -0500

    Added image scaling function

--- fetch.py
+++ fetch.py
@@ -5,9 +5,12 @@ from urllib2 import Request, urlopen as open_url
 from urlparse import urlsplit as split_url
 import re
 from os import listdir, remove
+from os.path import splitext
+import Image
 
 SOURCE_URL = "http://www.reddit.com/r/MapPorn/top/?sort=top&t=week"
 HEADERS = {'User-Agent' : 'Mozilla/5 (Linux i386) Gecko'}
+IMAGES = 'static/images/'
 
 def fetch_new_images():
 	html = _fetch()
@@ -36,18 +39,30 @@ def _cache_images(new_images):
 		request = Request(image['href'], headers=HEADERS)
 		response = open_url(request)
 		extracted_jpeg_data = response.read() 
-		f = open('static/images/' + new_filename, 'wb') #FIXME: catch exceptions?
+		f = open(IMAGES + new_filename, 'wb') #FIXME: catch exceptions?
 		f.write(extracted_jpeg_data)
 		f.close()
+		_scale_image(new_filename)
 	_purge_everything_but(new_images)
 
 def _purge_everything_but(new_images):
 	files_to_keep = set([_get_filename_from_url(image['href']) for image in new_images])
-	existing_files = set(listdir('static/images/')) 
+	existing_files = set(listdir(IMAGES)) 
 	files_to_remove = files_to_keep.difference(existing_files)
 	if files_to_remove:
 		for file_to_remove in files_to_remove:
-			remove('static/images/' + file_to_remove) 
+			remove(IMAGES + file_to_remove) 
 
 def _get_filename_from_url(url):
 	return split_url(url).path.split('/')[-1:][0]
+
+def _scale_image(image):
+	print "Scaling image", image
+	size = 700, 700
+	try:
+		im = Image.open(IMAGES + image)
+		im.thumbnail(size, Image.ANTIALIAS)
+		im.save(IMAGES + image)
+	except IOError as e:
+		print "Can't scale down image", image, "because:", e
+
